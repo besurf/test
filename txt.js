5817d9e2345f9c34dd747190d14af8b898e1d074
Shekhar Tyagi <shekhar.rdj@yahoo.com>
2017-03-13 20:43:20 +0530

    Update txt.js

--- txt.js
+++ txt.js
@@ -1,10 +1,6 @@
 $(document).ready(function() {
 $('#content').load("home.htm");
   //handle clicks
-  $('.txt').click(function(){
-  var page = $(this).attr('href');
-    alert(page);
-    $('#content').load(page);
-    return false;
-  });
+  if($('#about').click())
+  { $('#content').load('about.htm') }
 });
